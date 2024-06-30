2dc571db7b593d9e2d9edc0eb2b61c3763ec489d
Animalfox <a72e@outlook.com>
2024-06-30 05:18:51 +0300

    Add ReturnTaskToCurrentUseCase

---
+++ TodoApp/Core/Application/UseCases/ReturnTaskToCurrentUseCase.cs
@@ -0,0 +1,15 @@
+﻿using TodoApp.Core.Application.Services;
+
+namespace TodoApp.Core.Application.UseCases;
+
+public class ReturnTaskToCurrentUseCase(ITaskService taskService)
+{
+  public void Execute(int taskId)
+  {
+    var task = taskService.GetTaskById(taskId);
+    if (!task.IsCompleted) return;
+    task.IsCompleted = false;
+    taskService.UpdateTask(task);
+    // logic to discount points
+  }
+}
