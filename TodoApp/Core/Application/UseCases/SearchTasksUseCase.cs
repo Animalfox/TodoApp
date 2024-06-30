880e2828a43faab53a5a970ad26aa33c9c2612ad
Animalfox <a72e@outlook.com>
2024-06-30 05:22:39 +0300

    Add SearchTasksUseCase

---
+++ TodoApp/Core/Application/UseCases/SearchTasksUseCase.cs
@@ -0,0 +1,14 @@
+﻿using TodoApp.Core.Application.Services;
+using TodoApp.Core.Domain.Entities;
+
+namespace TodoApp.Core.Application.UseCases;
+
+public class SearchTasksUseCase(ITaskService taskService)
+{
+  public IEnumerable<TaskEntity> Execute(string query)
+  {
+    var tasks = taskService.GetAllTasks();
+    var result = tasks.Where(t => t.Title.Contains(query));
+    return result;
+  }
+}
