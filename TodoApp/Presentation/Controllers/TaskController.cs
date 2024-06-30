26a87d6653de5682abd261716cd68325316dc6c4
Animalfox <a72e@outlook.com>
2024-06-30 05:47:47 +0300

    Add TaskController

---
+++ TodoApp/Presentation/Controllers/TaskController.cs
@@ -0,0 +1,49 @@
+﻿using TodoApp.Core.Application.UseCases;
+using TodoApp.Core.Domain.Entities;
+
+namespace TodoApp.Presentation.Controllers;
+
+public class TaskController(
+  AddTaskUseCase addTaskUseCase,
+  EditTaskUseCase editTaskUseCase,
+  DeleteTaskUseCase deleteTaskUseCase,
+  ViewTasksUseCase viewTasksUseCase,
+  MarkTaskAsCompletedUseCase markTaskAsCompletedUseCase,
+  ReturnTaskToCurrentUseCase returnTaskToCurrentUseCase,
+  SearchTasksUseCase searchTasksUseCase)
+{
+  public void AddTask(TaskEntity task)
+  {
+    addTaskUseCase.Execute(task);
+  }
+
+  public void EditTask(TaskEntity task)
+  {
+    editTaskUseCase.Execute(task);
+  }
+
+  public void DeleteTask(int taskId)
+  {
+    deleteTaskUseCase.Execute(taskId);
+  }
+  
+  public IEnumerable<TaskEntity> ViewTasks()
+  {
+    return viewTasksUseCase.Execute();
+  }
+
+  public void MarkTaskAsCompleted(int taskId)
+  {
+    markTaskAsCompletedUseCase.Execute(taskId);
+  }
+
+  public void ReturnTaskToCurrent(int taskId)
+  {
+    returnTaskToCurrentUseCase.Execute(taskId);
+  }
+
+  public IEnumerable<TaskEntity> SearchTasks(string query)
+  {
+    return searchTasksUseCase.Execute(query);
+  }
+}
